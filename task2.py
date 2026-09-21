87145b2e7989f0980a08a6a73ae98239825203f0
KARAN <147038637+Karandevelopment@users.noreply.github.com>
2026-09-21 09:31:01 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,19 @@
+a=int(input("Enter first number :"))
+b=int(input("Enter second number : "))
+
+def add(a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mul(a,b):
+    return a * b
+def div(a,b):
+    return a / b
+def mod(a,b):
+    return a % b
+
+print(f"Addition: {add(a, b)}")
+print(f"Subtraction: {sub(a, b)}")
+print(f"Multiplication: {mul(a, b)}")
+print(f"Division: {div(a, b)}")
+print(f"Modulus: {mod(a, b)}")
